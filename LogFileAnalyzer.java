c11ee6f86d9b069e60a94a85ed994470974a2e7c
mloum1 <maniloum4@gmail.com>
2024-09-04 13:21:12 +0200

    updating class

---
+++ LogFileAnalyzer.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.io.*;
+
+public class LogFileAnalyzer {
+
+    public static List<String> readLogFile(String filePath) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            return reader.lines().collect(Collectors.toList());
+        }
+    }
+
+    public static String extractIPAddress(String logLine) {
+        return logLine.split("")[0]; // IP adress the data of each log line // keep hacking
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<String> loglines = readLogFile("dataset-dummy-data.log");
+        Map<String, Long> ipCounts = loglines.parallelStream()
+        .map(LogFileAnalyzer::extractIPAddress)
+        .collect(Collectors.groupingByConcurrent(ip -> ip, Collectors.counting()));
+
+        ipCounts.forEach((ip, count ) -> 
+        System.out.println("IP Address: " + ip + " - occurences: " + count));
+    }
+}
